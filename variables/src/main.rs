8e9ec04b9d9eb9e539b60bdec5d477c64b978882
Simon Lariz <simonlariz@gmail.com>
2023-08-27 19:16:15 -0700

    Add consts and shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,25 @@
 fn main() {
+    // Variables and Mutability
+    // Variables are immutable by default, but can be made mutable with the mut keyword
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    // Constants
+    // Constants are always immutable, and must be annotated with a type
+    // Naming convention is to use all uppercase with underscores between words
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // Shadowing
+    // Shadowing allows us to redeclare a variable with the same name
+    let y = 5;
+    let y = y + 1;
+
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+
+    println!("The value of y is: {y}");
 }
