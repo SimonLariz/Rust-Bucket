21e36805c6ba62f68a852bd77d7e67d34fe6c393
Simon Lariz <simonlariz@gmail.com>
2023-08-27 20:07:19 -0700

    fn with multiple parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -7,12 +7,20 @@ fn main() {
     println!("Hello, world!");
     another_function();
     another_function_with_parameter(8);
+    another_function_with_multiple_parameters(3, 'D');
 }
 
+// function definitions start with fn
 fn another_function() {
     println!("Another function.");
 }
 
+// functions can have parameters but must declare the type
 fn another_function_with_parameter(x: i32) {
     println!("The value of x is: {}", x);
+}
+
+// function with multiple parameters
+fn another_function_with_multiple_parameters(x: i32, y: char) {
+    println!("The value of x and y are: {x} and {y}");
 }
