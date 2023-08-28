c5271be7568ba8526bc3eedce01f98cb2a46ab61
Simon Lariz <simonlariz@gmail.com>
2023-08-27 21:36:30 -0700

    Add comments for understanding

--- temp-converter/src/main.rs
+++ temp-converter/src/main.rs
@@ -6,26 +6,32 @@ fn main() {
 
     let mut user_input = String::new();
 
+    // Loop until user enters "quit"
     while user_input.trim() != "quit"{
+        // Clear user_input
         user_input.clear();
         println!("Please enter a temperature in °F or 'quit' to quit.");
         print!(">");
         // flush() is needed to print! before read_line()
         io::stdout().flush().expect("Failed to flush stdout");
-        io::stdin().read_line(&mut user_input).expect("Failed to read line");
 
+        // Read user input and remove trailing newline
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+        // If user enters non-numeric input, continue loop
         let fahrenheit: f64 = match user_input.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
+        // Convert fahrenheit to celsius
         let celsius = fahrenheit_to_celsius(fahrenheit);
-        
+        // Print result
         println!("{}°F is {:.2}°C", fahrenheit, celsius)
     }
     println!("Goodbye!");
 }
 
+// Convert fahrenheit to celsius and return result
 fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
     (fahrenheit - 32.0) * 5.0 / 9.0
 }
