0b49c466ba706799448a327a6baac69b577815b0
Simon Lariz <simonlariz@gmail.com>
2023-08-27 19:08:34 -0700

    Add vars dir

---
+++ variables/target/debug/deps/variables-1a30770da882d5b0.d
@@ -0,0 +1,5 @@
+/Users/hydrogen/Desktop/Rust-Bucket/variables/target/debug/deps/variables-1a30770da882d5b0: src/main.rs
+
+/Users/hydrogen/Desktop/Rust-Bucket/variables/target/debug/deps/variables-1a30770da882d5b0.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/hydrogen/Desktop/Rust-Bucket/variables/target/debug/variables: /Users/hydrogen/Desktop/Rust-Bucket/variables/src/main.rs
